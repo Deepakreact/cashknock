bc3f70eabe9e39fb86c858824b252f68c8347122
Deepakreact <deepaksharmaosho@gmail.com>
2022-07-10 17:41:59 +0530

    k

--- backend/settings.py
+++ backend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cash'
+    'cash',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
